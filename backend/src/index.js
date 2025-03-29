8026db02fde961fd5759d7527d1374a0f905fa76
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-03-29 17:28:56 +0530

    signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,17 @@ import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
+import cors from "cors"
 
 dotenv.config();
 const app=express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 const PORT=process.env.PORT;
 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,50 @@
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore=create((set)=>({
+    authUser:null,
+    isSigningUp:false,
+    isLoggingIn:false,
+    isUpdatingProfile:false,
+
+    isCheckingAuth:true,
+    checkAuth:async()=>{
+        try{
+            const res=await axiosInstance.get("/auth/check")
+            set({authUser:res.data})
+        }
+        catch(error){
+            console.log("Error in CheckAuth ",error)
+            set({authUser:null});
+        }
+        finally{
+            set({isCheckingAuth:false})
+        }
+    },
+    signUp:async(data)=>{
+        set({isSigningUp:true})
+        try{
+            const res=await axiosInstance.post("/auth/signup",data)
+            set({authUser:res.data})
+            toast.success("Account Created Successfully")
+
+        }
+        catch(error){
+            toast.error(error.response.data.message)
+        }
+        finally{
+            set({isSigningUp:false})
+        }
+    },
+    logout:async()=>{
+        try{
+            await axiosInstance.post("/auth/logout")
+            set({authUser:null})
+            toast.success("Logout Successfully!!")
+        }
+        catch(error){
+            toast.error(error.response.data.message)
+        }
+    }
+}))
