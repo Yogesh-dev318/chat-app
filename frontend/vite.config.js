80100dd0caed302c1361fc1465efeb114ece161d
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-03-30 17:54:47 +0530

    added settings page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import daisyui from "daisyui"
-import themes from "daisyui/theme/object";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  daisyui:{
-    themes:[
-      "light",
-      "dark",
-      "cupcake",
-      "retro"
-    ]
-  }
+  
 });
