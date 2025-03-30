c5d203b98f0fa1cb329d91f6fffd0e5fbab70c0a
Yogesh-dev318 <yogeshpoonia2020@gmail.com>
2025-03-30 17:01:42 +0530

    added profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import cors from "cors"
 dotenv.config();
 const app=express();
 
-app.use(express.json())
+app.use(express.json({limit:'50mb'}))
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
